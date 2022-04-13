17233a8a4c8aa6e19ccbced1e5b77d8fc78cb749
Guille-Rubio <guillermorubiog@gmail.com>
2022-04-14 00:42:59 +0200

    ADD product scheme

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const router = require('./routes/routes')
-require('./config/mongoConfig')
+const express = require('express');
+const morgan = require('./config/morganConfig');
+const app = express();
+const port = 3000;
+const router = require('./routes/routes');
+require('./config/mongoConfig');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'pug');
 app.set('views', './views');
+app.use(morgan(':method :host :status :param[id] - :response-time ms :body'));
 
 app.use('/', router);
 
---
+++ models/product.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+
+    id_product: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    product_name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    product_description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    imgURL: {
+        type: String,
+        required: true,
+        trim: true
+    }
+})
+
+
+const productModel = mongoose.model('products', ProductSchema);
+
+module.exports = productModel;
